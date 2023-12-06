21776d5ad2915af4f0458801d91a8b6209ea064f
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-07 00:27:25 +0100

    setting up LI elements

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,17 +1,21 @@
-import styles from "./List.module.css";
+import "../../App.css";
 import liItems from "./listItems.json";
 
 export default function List() {
   return (
     <>
-      <ul className={styles.ul}>
+      <ul className="list">
         {liItems.map((item) => (
-          <li key={item.text} className={styles.li}>
+          <li key={item.text} className="listItem">
+            <img
+              src="src/assets/images/icon-list.svg"
+              alt="list-icons"
+              width="18px"
+            />
             {item.text}
           </li>
         ))}
       </ul>
-      ;
     </>
   );
 }
