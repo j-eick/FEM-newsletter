4ff5fcb9b680067419f5efdaeef7a6c2e4bb7335
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-07 00:25:28 +0100

    Basic newsletter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import Input from "./components/input";
+import List from "./components/list";
 
 function App() {
-  return <>hello gello</>;
+  return (
+    <div className="container">
+      <div className="left">
+        <h1 className="title">Stay updated!</h1>
+        <p className="description">
+          Join 60.000+ product managers receiving monthly updates on:
+        </p>
+        <List />
+        <Input />
+      </div>
+      <div className="right">
+        <img
+          className="img"
+          src="src/assets/images/illustration-sign-up-desktop.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
